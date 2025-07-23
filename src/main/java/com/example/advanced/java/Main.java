c921bd780da0b29a7533b0a93d6b5c9179807fe1
pedro lopes <tecpedrohl@gmail.com>
2025-07-23 17:33:34 +0100

    Reading files improved java9

--- src/main/java/com/example/advanced/java/Main.java
+++ src/main/java/com/example/advanced/java/Main.java
@@ -1,7 +1,23 @@
 package com.example.advanced.java;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("file.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+
+        try(br){
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+
+
     }
 }
